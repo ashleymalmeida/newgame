18b3dec267af5c6c390ad104b5c2dbb9e6e40cad
ashleymalmeida <almeida20082017@gmail.com>
2025-05-12 13:32:18 -0700

    new

---
+++ flappybird.js
@@ -0,0 +1,15 @@
+
+//board
+let board;
+let boardWidth = 360;
+let boardHeight = 640;
+let context;
+
+window.onload = function () {
+    board = document.getElementById("Board");
+    board.height = boardHeight;
+    board.width = boardWidth;
+    context = board.getContext("2d"); //used for drawing on the board
+    
+}
+
