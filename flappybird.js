9fe2fb8d4b1401db59237d5516ab69f3a3b5f95e
ashleymalmeida <almeida20082017@gmail.com>
2025-05-12 14:11:49 -0700

    new101

--- flappybird.js
+++ flappybird.js
@@ -1,15 +1,31 @@
-
 //board
 let board;
 let boardWidth = 360;
-let boardHeight = 640;
+let boardHeight = 650;
 let context;
 
+//bird
+let bridWidth = 34; //width/height ratio = 408228 = 17/12
+let birdHeight = 24;
+let birdX = boardWidth/8;
+let birdY = boardHeight/2;
+
+let bird = {
+    x : birdX,
+    y : birdY,
+    width : bridWidth,
+    height : birdHeight
+}
+
 window.onload = function () {
-    board = document.getElementById("Board");
+    board = document.getElementById("board"); // lowercase
     board.height = boardHeight;
     board.width = boardWidth;
     context = board.getContext("2d"); //used for drawing on the board
     
+
+    //draw the flappy bird
+    context.fillstyle = "green";
+    context.fillRect(bird.x, bird.y, bird.width, bird.height);
 }
 
